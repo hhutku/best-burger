1ff4c1baccf752437eb892fde37747ff897b7c60
hhutku <hhutku.wa@gmail.com>
2021-01-11 14:52:01 -0800

    orm helper functions added

---
+++ config/orm.js
@@ -0,0 +1,29 @@
+const connection = require('./connection');
+
+
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+// turning an object into something sequel can understand
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+
+module.exports = orm;
